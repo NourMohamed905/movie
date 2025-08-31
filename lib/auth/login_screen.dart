964265aa426f44c84355d440f5a8a23361aa8df4
Ahmad Sameer <wizahmad88@gmail.com>
2025-08-31 16:14:39 +0400

    add file Login Screen

---
+++ lib/auth/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String routeName = 'Login-screeen';
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [Text('data')],);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie/auth/login_screen.dart';
 import 'package:movie/home.dart';
 
 void main() {
@@ -9,6 +10,6 @@ class Movies extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(routes: {Home.routeName: (_) => Home()});
+    return MaterialApp(routes: {Home.routeName: (_) => Home() , LoginScreen.routeName:(_)=> LoginScreen()});
   }
 }
